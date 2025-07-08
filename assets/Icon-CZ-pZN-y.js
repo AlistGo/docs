ec8bac5f11e746b9b53dd3040f07f5a1379b1047
JoaHuang <JoaHuang@users.noreply.github.com>
2025-07-08 03:38:11 +0000

    deploy: d845ef233005634d45fc6eac382b161613033af5

--- assets/Icon-CZ-pZN-y.js
+++
@@ -1,9 +0,0 @@
-import{p as f,q as l,s as d,d as v,v as y,h,x as b,y as C,z as m,A as z,B as a,C as _,D as $}from"./app-DTBURrV0.js";const x=f("icon",`
- height: 1em;
- width: 1em;
- line-height: 1em;
- text-align: center;
- display: inline-block;
- position: relative;
- fill: currentColor;
-`,[l("color-transition",{transition:"color .3s var(--n-bezier)"}),l("depth",{color:"var(--n-color)"},[d("svg",{opacity:"var(--n-opacity)",transition:"opacity .3s var(--n-bezier)"})]),d("svg",{height:"1em",width:"1em"})]),R=Object.assign(Object.assign({},m.props),{depth:[String,Number],size:[Number,String],color:String,component:[Object,Function]}),P=v({_n_icon__:!0,name:"Icon",inheritAttrs:!1,props:R,setup(e){const{mergedClsPrefixRef:o,inlineThemeDisabled:t}=C(e),s=m("Icon","-icon",x,z,e,o),r=a(()=>{const{depth:i}=e,{common:{cubicBezierEaseInOut:c},self:u}=s.value;if(i!==void 0){const{color:p,[`opacity${i}Depth`]:g}=u;return{"--n-bezier":c,"--n-color":p,"--n-opacity":g}}return{"--n-bezier":c,"--n-color":"","--n-opacity":""}}),n=t?_("icon",a(()=>`${e.depth||"d"}`),r,e):void 0;return{mergedClsPrefix:o,mergedStyle:a(()=>{const{size:i,color:c}=e;return{fontSize:$(i),color:c}}),cssVars:t?void 0:r,themeClass:n==null?void 0:n.themeClass,onRender:n==null?void 0:n.onRender}},render(){var e;const{$parent:o,depth:t,mergedClsPrefix:s,component:r,onRender:n,themeClass:i}=this;return!((e=o==null?void 0:o.$options)===null||e===void 0)&&e._n_icon__&&y("icon","don't wrap `n-icon` inside `n-icon`"),n==null||n(),h("i",b(this.$attrs,{role:"img",class:[`${s}-icon`,i,{[`${s}-icon--depth`]:t,[`${s}-icon--color-transition`]:t!==void 0}],style:[this.cssVars,this.mergedStyle]}),r?h(r):this.$slots)}});export{P as N};
